a01aaf3b8ab1310c9be6e7cff681ce2bd5c2f96c
Cal Henderson <cal@iamcal.com>
2010-07-13 01:54:02 +0000

    much better options handling. save window position. started on some cooldown calculations
    git-svn-id: https://svn.iamcal.com/public/wow/BeeTeam/trunk@5673 ac7c8259-0de0-4f3c-bb6a-12394caf8efd

--- BeeTeam.lua
+++ BeeTeam.lua
@@ -3,6 +3,22 @@ BT = {}
 BT.fullW = 160;
 BT.fullH = 100;
 
+BT.priorities = {
+	p1 = "serp",
+	p2 = "chim",
+};
+
+BT.abilities = {
+	serp = {
+		icon = [[ability_hunter_quickshot]],
+		cooldown = function() return BT.DebuffElseGCD('Serpent Sting'); end,
+	},
+	chim = {
+		icon = [[ability_hunter_chimerashot2]],
+		cooldown = function() return BT.SimpleGCD(); end,
+	},
+}
+
 
 function BT.OnLoad()
 
@@ -27,11 +43,7 @@ end
 function BT.OptionClick(button, name)
 
 	if (name == 'hide') then
-		if (_G.BeeTeamDB.opts.hide) then
-			BT.Show();
-		else
-			BT.Hide();
-		end
+		BT.ToggleHide();
 	end
 
 	if (name == 'lock') then
@@ -43,6 +55,10 @@ end
 
 function BT.OnSaving()
 
+	local point, relativeTo, relativePoint, xOfs, yOfs = BT.UIFrame:GetPoint()
+	_G.BeeTeamDB.opts.frameRef = relativePoint;
+	_G.BeeTeamDB.opts.frameX = xOfs;
+	_G.BeeTeamDB.opts.frameY = yOfs;
 end
 
 
@@ -93,7 +109,6 @@ function BT.StartFrame()
 	BT.UIFrame.texture:SetAllPoints(BT.UIFrame)
 	BT.UIFrame.texture:SetTexture(0, 0, 0)
 
-
 	-- position the parent frame
 	local frameRef = "CENTER";
 	local frameX = 0;
@@ -102,8 +117,6 @@ function BT.StartFrame()
 		frameRef = _G.BeeTeamDB.opts.frameRef;
 		frameX = _G.BeeTeamDB.opts.frameX;
 		frameY = _G.BeeTeamDB.opts.frameY;
-
-		BT.UIFrame:SetWidth(_G.BeeTeamDB.opts.frameW);
 	end
 	BT.UIFrame:SetPoint(frameRef, frameX, frameY);
 
@@ -111,17 +124,7 @@ function BT.StartFrame()
 	BT.UIFrame:SetMovable(true);
 	BT.UIFrame:EnableMouse(true);
 
-	-- create the button
-	--local d2 = BT.ItemData(_G.BeeTeamDB.opts.curItem);
-	BT.Button = CreateFrame("Button", nil, BT.UIFrame)
-	BT.Button:SetPoint("TOPLEFT", 1, -1)
-	BT.Button:SetWidth(30)
-	BT.Button:SetHeight(30)
-	--BT.Button:SetNormalTexture(d2.itemTexture);
-	BT.Button:SetScript("OnClick", BT.ShowMenu)
-
-
-	-- some text to go over it
+	-- some text
 	BT.Label = BT.UIFrame:CreateFontString(nil, "OVERLAY", "GameFontHighlightSmall")
 	BT.Label:SetPoint("CENTER", BT.UIFrame, "CENTER", 2, 0)
 	BT.Label:SetJustifyH("LEFT")
@@ -129,8 +132,7 @@ function BT.StartFrame()
 	BT.Label:SetTextColor(1,1,1,1)
 	BT.SetFontSize(BT.Label, 10)
 
-
-	-- aspect button
+	-- buttons!
 	BT.BtnRot1 = BT.CreateButton(0, 0, 40, 40, [[Interface\Icons\ability_hunter_pet_dragonhawk]]);
 	BT.BtnRot2 = BT.CreateButton(40, 0, 40, 40, [[Interface\Icons\ability_hunter_pet_dragonhawk]]);
 	BT.BtnRot3 = BT.CreateButton(80, 0, 40, 40, [[Interface\Icons\ability_hunter_pet_dragonhawk]]);
@@ -159,14 +161,8 @@ function BT.StartFrame()
 
 
 
-
-	BT.UpdateFrame();
-
-	if (_G.BeeTeamDB.opts.hide) then
-		BT.UIFrame:Hide();
-	else
-		BT.UIFrame:Show();
-	end
+	BT.SetLocked(_G.BeeTeamDB.opts.locked);
+	BT.SetHide(_G.BeeTeamDB.opts.hide);
 end
 
 function BT.ShowMenu()
@@ -197,7 +193,7 @@ function BT.ShowMenu()
 
 	table.insert(menuList, {
 		text = "Hide Window",
-		func = function() BT.Hide() end,
+		func = function() BT.SetHide(true) end,
 		isTitle = false,
 		checked = false,
 		disabled = false,
@@ -218,9 +214,30 @@ function BT.CreateButton(x, y, w, h, texture)
 	return b;
 end
 
-function BT.UpdateFrame()
+function BT.RebuildFrame()
+
+	BT.BtnRot1:SetNormalTexture([[Interface\Icons\]] .. BT.abilities[BT.priorities.p1].icon);
+	BT.BtnRot2:SetNormalTexture([[Interface\Icons\]] .. BT.abilities[BT.priorities.p2].icon);
 
 end
+
+function BT.UpdateFrame()
+
+	local t1 = BT.abilities[BT.priorities.p1].cooldown();
+	local t2 = BT.abilities[BT.priorities.p2].cooldown();
+
+	BT.Label:SetText(string.format("%0.2f, %0.2f", t1, t2))
+
+end
+
+function BT.OnUpdate()
+	if (_G.BeeTeamDB.opts.hide) then 
+		return;
+	end
+
+	BT.UpdateFrame();
+end
+
 
 function BT.SetFontSize(string, size)
 
@@ -228,16 +245,17 @@ function BT.SetFontSize(string, size)
 	string:SetFont(Font, size, Flags)
 end
 
-function BT.Show()
-	_G.BeeTeamDB.opts.hide = false;
-	BT.UIFrame:Show();
-	BTOptionsFrameCheck1:SetChecked(true);
-end
-
-function BT.Hide()
-	_G.BeeTeamDB.opts.hide = true;
-	BT.UIFrame:Hide();
-	BTOptionsFrameCheck1:SetChecked(false);
+function BT.SetHide(a)
+	_G.BeeTeamDB.opts.hide = a;
+	if (a) then
+		BT.UIFrame:Hide();
+		BTOptionsFrameCheck1:SetChecked(false);
+	else
+		BT.UIFrame:Show();
+		BTOptionsFrameCheck1:SetChecked(true);
+	end
+	BT.RebuildFrame();
+	BT.UpdateFrame();
 end
 
 function BT.SetLocked(a)
@@ -262,25 +280,45 @@ end
 
 function BT.ToggleHide()
 	if (_G.BeeTeamDB.opts.hide) then
-		BT.Show();
+		BT.SetHide(false);
 	else
-		BT.Hide();
+		BT.SetHide(true);
 	end
 end
 
-function BT.OnUpdate()
+function BT.SimpleGCD()
 
-	if (BT.inSession and GetTime() > BT.sessionEnds) then
-
-		BT.EndSession();
+	local start, duration = GetSpellCooldown(1462);
+	if duration > 0 then
+		return start + duration - GetTime()
 	end
+
+	return 0
+end
+
+function BT.DebuffElseGCD(aName)
+	local index = 1
+	while UnitDebuff("target", index) do
+		local name, _, _, count, _, _, debuffExpires, caster = UnitDebuff("target", index)
+		if ((name == aName) and (caster == "player")) then
+			return debuffExpires - GetTime()
+		end
+		index = index + 1
+	end
+
+	local start, duration = GetSpellCooldown(1462);
+	if duration > 0 then
+		return start + duration - GetTime()
+	end
+
+	return 0
 end
 
 function BT.SlashCommand(msg, editbox)
 	if (msg == 'show') then
-		BT.Show();
+		BT.SetHide(false);
 	elseif (msg == 'hide') then
-		BT.Hide();
+		BT.SetHide(true);
 	elseif (msg == 'toggle') then
 		BT.ToggleHide();
 	elseif (msg == 'reset') then
