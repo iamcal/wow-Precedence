045fbdb95f78b70e082e94d5d4e2ad5448caeb61
Cal Henderson <cal@iamcal.com>
2011-12-28 12:38:46 -0800

    added a warning for the guild teleport cloak (which i keep forgetting to unequip)

--- Precedence.lua
+++ Precedence.lua
@@ -98,6 +98,7 @@ PREC.default_options = {
 		growl_solo = true,
 		growl_party = true,
 		kirin_tor_ring = false,
+		guild_cloak = false,
 	},
 };
 
@@ -339,6 +340,10 @@ PREC.warningdefs = {
 	kirin_tor_ring = {
 		title = "Kirin Tor Ring Equipped",
 		icon = [[Interface\Icons\inv_jewelry_ring_74]],
+	},
+	guild_cloak = {
+		title = "Guild Cloak Equipped",
+		icon = [[Interface\Icons\inv_guild_cloak_alliance_a]],
 	},
 };
 
@@ -1974,6 +1979,18 @@ function PREC.GetWarning(key, info)
 		if (itemId1 == 48954 or itemId2 == 48954) then info.show = true; end
 		if (itemId1 == 45688 or itemId2 == 45688) then info.show = true; end
 		if (itemId1 == 40586 or itemId2 == 40586) then info.show = true; end
+	end
+
+	if (key == "guild_cloak") then
+
+		local itemId = GetInventoryItemID("player", 15);
+
+		if (itemId == 63352) then info.show = true; end -- alliance 8hr
+		if (itemId == 63353) then info.show = true; end -- horde 8hr
+		if (itemId == 63206) then info.show = true; end -- alliance 4hr
+		if (itemId == 63207) then info.show = true; end -- horde 4hr
+		if (itemId == 65360) then info.show = true; end -- alliance 2hr
+		if (itemId == 65274) then info.show = true; end -- horde 2hr
 	end
 
 	return info;
